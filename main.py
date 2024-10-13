61df1bc9b89221b934777c7874354c01d8bb82b5
Ruben Hernandez <hernandzruben9@gmail.com>
2024-10-12 21:24:12 -0400

    ddd

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 
-from querys.Query import Query
+from querys.Query import Query  # Asegúrate de que este archivo esté bien
 
 # Crear el esquema de GraphQL
 schema = strawberry.Schema(query=Query)
@@ -11,17 +10,20 @@ schema = strawberry.Schema(query=Query)
 # Configurar la aplicación FastAPI
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Hello Worddd"}
 
-
 @app.get("/test")
-async def roota():
-    return {"message": "Hello Worddd"}
-
+async def test():
+    return {"message": "This is a test"}
 
 # Registrar la ruta GraphQL con GraphiQL habilitado
-graphql_app = GraphQLRouter(schema, graphiql=True)  # Habilitar GraphiQL
-app.include_router(graphql_app, prefix="/graphql")  # Sin el prefijo "/graphql"
+graphql_app = GraphQLRouter(schema, graphiql=True)
+app.include_router(graphql_app, prefix="/graphql")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8080)
