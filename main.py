43b2d82900bb13cea1a998be9234b2af6ecd4cc3
Ruben Hernandez <hernandzruben9@gmail.com>
2024-10-22 22:33:14 -0400

    Consulta e Insertar de Piezas

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ app.lifespan = lifespan
 
 def start_server():
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
 
 if __name__ == '__main__':
     start_server()
