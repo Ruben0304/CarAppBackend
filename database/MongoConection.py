f89ff9b3edbf4c7080996ce3065f9f44e9838d27
Ruben Hernandez <hernandzruben9@gmail.com>
2024-10-22 21:46:08 -0400

    Consulta e Insertar de Piezas

--- database/MongoConection.py
+++ database/MongoConection.py
@@ -1,6 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 class MongoDB:
+   @staticmethod
    async def get_database(self):
        client = AsyncIOMotorClient("mongodb+srv://ruben:zixelowe1@personal.yycznyk.mongodb.net/")
        return client.CarApp
